1d2bedd21f5fe1eb55272b4a10a49aed80cf5fce
danieloh30 <doh@redhat.com>
2021-06-08 16:11:23 -0400

    Update code

--- bin/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into person (id, name, address) values (nextval('hibernate_sequence'), 'Dan', 'Boston');
-insert into person (id, name, address) values (nextval('hibernate_sequence'), 'James', 'Raleigh');
-insert into person (id, name, address) values (nextval('hibernate_sequence'), 'Eric', 'LA');
-insert into person (id, name, address) values (nextval('hibernate_sequence'), 'Justin', 'Seoul');
